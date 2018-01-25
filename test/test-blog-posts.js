f54174648ea6152572e0c6014711beec86a49baf
Jake Betcher <jakebetcher@gmail.com>
2018-01-25 11:51:17 -0800

    add PUT, POST, and DELETE integration tests

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -38,17 +38,22 @@ function seedBlogPostData() {
   console.info('seeding blog post data');
   const seedData = [];
   for (let i = 1; i <= 10; i++) {
-    seedData.push({
-      author: {
+    seedData.push(generateBlogData());
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
+function generateBlogData() {
+  return {
+    author: {
         firstName: faker.name.firstName(),
         lastName: faker.name.lastName()
       },
       title: faker.lorem.sentence(),
       content: faker.lorem.text()
-    });
-  }
-  // this will return a promise
-  return BlogPost.insertMany(seedData);
+    };
+  
 }
 
 
@@ -129,5 +134,92 @@ describe('blog posts API resource', function () {
     });
 });
 
+describe('POST Endpoint', function() {
+  it('it should add a new blog post', function() {
+    const newBlogPost = generateBlogData();
+
+    return chai.request(app)
+    .post('/posts')
+    .send(newBlogPost)
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('id', 'author', 'title', 'content', 'created');
+      res.body.author.should.contain(newBlogPost.author.firstName);
+      res.body.author.should.contain(newBlogPost.author.lastName);
+      res.body.title.should.equal(newBlogPost.title);
+      res.body.content.should.equal(newBlogPost.content);
+
+      return BlogPost.findById(res.body.id);
+    })
+    .then(function(post) {
+        post.title.should.equal(newBlogPost.title);
+        post.content.should.equal(newBlogPost.content);
+        post.author.firstName.should.equal(newBlogPost.author.firstName);
+        post.author.lastName.should.equal(newBlogPost.author.lastName);
+    });
+  });
+
+});
+
+describe('PUT Endpoint', function() {
+
+  it('should update fields you send over', function() {
+      const updateData = {
+        title: 'foo',
+        content: 'bbbbbbbbbggdsghgdsgfjhdsgfhjdsgfhjdgfhjdsgfjhdgfhjdsgfjsdhgfjhdsgfhjdsgfjhdsgfhjdgjhdgfhjgfhdgfjhdgfjhdgfhjdgfhjfghjdgfhdjgfhhd'
+      };
+
+      return BlogPost
+        .findOne()
+        .then(function(post) {
+          updateData.id = post.id;
+
+          // make request then inspect it to make sure it reflects
+          // data we sent
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+
+          return BlogPost.findById(updateData.id);
+        })
+        .then(function(post) {
+          post.title.should.equal(updateData.title);
+          post.content.should.equal(updateData.content);
+        });
+    });
+});
+
+
+
+  describe('DELETE endpoint', function() {
+    // strategy:
+    //  1. get a restaurant
+    //  2. make a DELETE request for that restaurant's id
+    //  3. assert that response has right status code
+    //  4. prove that restaurant with the id doesn't exist in db anymore
+    it('delete a blog post by id', function() {
+
+      let post;
+
+      return BlogPost
+        .findOne()
+        .then(function(_post) {
+          post = _post;
+          return chai.request(app).delete(`/posts/${post.id}`);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+          return BlogPost.findById(post.id);
+        })
+        .then(function(_post) {
+          should.not.exist(_post);
+        });
+    });
+  });
 
 });
