09ffbc10c80a9193088b55a1ca6692dbf20e66f6
Jake Betcher <jakebetcher@gmail.com>
2018-01-25 11:17:36 -0800

    complete GET integration test

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -5,95 +5,129 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 
+// this makes the should syntax available throughout
+// this module
+const should = chai.should();
 
-const expect = chai.expect;
-
-const {BlogPost} = require('../models');
-const {app, runServer, closeServer} = require('../server');
-const {Test_DATABASE_URL} = require('../config');
+const { BlogPost } = require('../models');
+const { closeServer, runServer, app } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
 
-function seedBlogPostData() {
-	console.info('seeding blog post data');
-	const seedData = [];
-	for (let i = 1; i <= 10; i++) {
-		seedData.push(generateBlogData());
-	}
-	return BlogPost.insertMany(seedData);
-}
-
-
-function generateBlogData() {
-	return {
-		author: {
-			firstName: faker.name.firstName(),
-			lastName: faker.name.lastName()
-		},
-		title: faker.lorem.sentence(),
-		content: faker.lorem.paragraphs(),
-		created: faker.date.past()
-	}
-}
-
+// this function deletes the entire database.
+// we'll call it in an `afterEach` block below
+// to ensure  ata from one test does not stick
+// around for next one
 function tearDownDb() {
-  console.warn('Deleting database');
-  return mongoose.connection.dropDatabase();
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
 }
 
-describe('Blog Posts API Resource', function() {
-	before(function() {
-    return runServer(Test_DATABASE_URL);
+
+// used to put randomish documents in db
+// so we have data to work with and assert about.
+// we use the Faker library to automatically
+// generate placeholder values for author, title, content
+// and then we insert that data into mongo
+function seedBlogPostData() {
+  console.info('seeding blog post data');
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push({
+      author: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName()
+      },
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text()
+    });
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
+
+describe('blog posts API resource', function () {
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
   });
 
-  beforeEach(function() {
+  beforeEach(function () {
     return seedBlogPostData();
   });
 
-  afterEach(function() {
+  afterEach(function () {
+    // tear down database so we ensure no state from this test
+    // effects any coming after.
     return tearDownDb();
   });
 
-  after(function() {
+  after(function () {
     return closeServer();
   });
 
-describe('GET endpoint', function() {
+  // note the use of nested `describe` blocks.
+  // this allows us to make clearer, more discrete tests that focus
+  // on proving something small
+  describe('GET endpoint', function () {
 
-    it('should return all existing blog posts', function() {
+    it('should return all existing posts', function () {
       // strategy:
-      //    1. get back all restaurants returned by by GET request to `/restaurants`
+      //    1. get back all posts returned by by GET request to `/posts`
       //    2. prove res has right status, data type
-      //    3. prove the number of restaurants we got back is equal to number
+      //    3. prove the number of posts we got back is equal to number
       //       in db.
-      //
-      // need to have access to mutate and access `res` across
-      // `.then()` calls below, so declare it here so can modify in place
       let res;
       return chai.request(app)
         .get('/posts')
-        .then(function(_res) {
-          // so subsequent .then blocks can access response object
+        .then(_res => {
           res = _res;
-          expect(res).to.have.status(200);
-           //otherwise our db seeding didn't work
-          expect(res.body.posts).to.have.length.of.at.least(1);
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.should.have.length.of.at.least(1);
+          console.log(res.body);
           return BlogPost.count();
         })
-        .then(function(count) {
-          expect(res.body.posts).to.have.length.of(count);
+        .then(count => {
+          // the number of returned posts should be same
+          // as number of posts in DB
+          res.body.should.have.length(count);
         });
-   
+    });
 
 
-  });
+    it('should return posts with the right fields', function() {
+      let resBlogPosts;
+      return chai.request(app)
+      .get('/posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.have.length.of.at.least(1);
 
- });
+        res.body.forEach(function(post) {
+          post.should.be.a('object');
+          post.should.include.keys('id', 'author', 'title', 'content', 'created');
+        });
+        resBlogPosts = res.body[0];
+        return BlogPost.findById(resBlogPosts.id);
+      })
+      .then(function(post) {
+        resBlogPosts.id.should.equal(post.id);
+        resBlogPosts.author.should.contain(post.author.firstName);
+        resBlogPosts.author.should.contain(post.author.lastName);
+        resBlogPosts.title.should.equal(post.title);
+        resBlogPosts.content.should.equal(post.content);
+      });
+    });
+});
 
-  });
-  
 
-
-
-
-
+});
